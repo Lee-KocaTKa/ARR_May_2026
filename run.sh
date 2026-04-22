29829cd9dbb55c17abceba07304a3369ac79652d
Lee-KocatKa <leesheenon@gmail.com>
2026-04-22 15:42:35 +0000

    model code for gemma model

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=llava_anaph
 #SBATCH --partition=public
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --time=12:00:00
 #SBATCH --chdir=/mnt/home/sangmyeong-l/research/ARR_May_2026
 #SBATCH -o slurm-%j.out
 
 export PYTHONPATH=/mnt/home/sangmyeong-l/research/ARR_May_2026/src
-python -m scripts.run_llama4_eval --category pp
+python -m scripts.run_gemma --category pp
